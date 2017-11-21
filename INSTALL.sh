851ebbfb4fd12ee9e0dab9f759edc6c46d9b9d41
emmett1 <emmett1.2miligrams@gmail.com>
2017-11-21 23:51:18 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,10 +4,11 @@ BINDIR=${BINDIR:-/usr/bin}
 
 FUNCDIR=/usr/share/scratchpkg
 CONFDIR=/etc
+HOOK_DIR=/etc/hooks
 HTTPUPDIR=/etc/ports
 VAR_DIR=/var/spkg
 
-mkdir -pv ${DESTDIR}{${BINDIR},${FUNCDIR},${CONFDIR},${HTTPUPDIR}}
+mkdir -pv ${DESTDIR}{${BINDIR},${FUNCDIR},${CONFDIR},${HOOK_DIR},${HTTPUPDIR}}
 install -m755 buildpkg installpkg removepkg scratch ${DESTDIR}${BINDIR}
 install -m644 functions/{color,functions,options} ${DESTDIR}${FUNCDIR}
 install -m644 scratchpkg.conf ${DESTDIR}${CONFDIR}/scratchpkg.conf.orig
