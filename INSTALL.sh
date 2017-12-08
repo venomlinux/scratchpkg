6bdefe7b96b59cba0cc8f371194a0516ce3e0b4e
emmett1 <emmett1.2miligrams@gmail.com>
2017-12-08 21:15:28 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -10,7 +10,7 @@ VAR_DIR=/var/spkg
 
 mkdir -pv ${DESTDIR}{${BINDIR},${FUNCDIR},${CONFDIR},${HOOK_DIR},${HTTPUPDIR}}
 install -m755 buildpkg installpkg removepkg scratch ${DESTDIR}${BINDIR}
-install -m644 functions/{color,functions,options} ${DESTDIR}${FUNCDIR}
+install -m644 functions/* ${DESTDIR}${FUNCDIR}
 install -m644 scratchpkg.conf ${DESTDIR}${CONFDIR}/scratchpkg.conf.orig
 install -m644 ports/{core,extra,xorg}.httpup ${DESTDIR}${HTTPUPDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
