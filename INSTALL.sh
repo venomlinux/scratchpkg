aa33c479a84eed0783de3895b946811036d4b8b3
emmett1 <emmett1.2miligrams@gmail.com>
2018-06-03 16:58:27 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,15 +4,13 @@ BINDIR=/usr/bin
 FUNCDIR=/usr/share/scratchpkg
 CONFDIR=/etc
 HOOK_DIR=/etc/hooks
-HTTPUPDIR=/etc/ports
 CACHE_DIR=/var/cache/scratchpkg
 INDEX_DIR=/var/lib/scratchpkg
 PORT_DIR=/usr/ports
 
-mkdir -pv ${DESTDIR}{${BINDIR},${FUNCDIR},${CONFDIR},${HOOK_DIR},${HTTPUPDIR},${PORT_DIR}}
+mkdir -pv ${DESTDIR}{${BINDIR},${FUNCDIR},${CONFDIR},${HOOK_DIR},${PORT_DIR}}
 install -m755 buildpkg installpkg removepkg scratch ${DESTDIR}${BINDIR}
 install -m644 functions/* ${DESTDIR}${FUNCDIR}
 install -m644 scratchpkg.conf ${DESTDIR}${CONFDIR}/scratchpkg.conf
-install -m644 ports/*.httpup ${DESTDIR}${HTTPUPDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
-mkdir -p ${DESTDIR}${CACHE_DIR}/{log,packages,sources} ${DESTDIR}${INDEX_DIR}/index
+mkdir -pv ${DESTDIR}${CACHE_DIR}/{log,packages,sources} ${DESTDIR}${INDEX_DIR}/index
