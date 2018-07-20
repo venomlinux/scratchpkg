1169c8886d50240fe649727b96e208843fda04d5
emmett1 <emmett1.2miligrams@gmail.com>
2018-07-20 18:30:42 +0800

    major update

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 BINDIR=/usr/bin
-FUNCDIR=/usr/share/scratchpkg
 CONFDIR=/etc
 HOOK_DIR=/etc/hooks
 SYSUSERS_DIR=/etc/sysusers
@@ -9,9 +8,8 @@ CACHE_DIR=/var/cache/scratchpkg
 INDEX_DIR=/var/lib/scratchpkg
 PORT_DIR=/usr/ports
 
-mkdir -pv ${DESTDIR}{${BINDIR},${FUNCDIR},${CONFDIR},${HOOK_DIR},${SYSUSERS_DIR},${PORT_DIR}}
-install -m755 revdep buildpkg installpkg removepkg scratch ${DESTDIR}${BINDIR}
-install -m644 functions/* ${DESTDIR}${FUNCDIR}
+mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${HOOK_DIR},${SYSUSERS_DIR},${PORT_DIR}}
+install -m755 revdep pkgadd pkgdel pkgbuild pkgcreate pkgdeplist scratch ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
-mkdir -pv ${DESTDIR}${CACHE_DIR}/{log,packages,sources} ${DESTDIR}${INDEX_DIR}/index
+mkdir -pv ${DESTDIR}${CACHE_DIR}/{packages,sources,work} ${DESTDIR}${INDEX_DIR}/index
