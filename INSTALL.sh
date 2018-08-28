71f6c7b221f71ce15cf5ef833ccf971f7bf6eba5
emmett1 <emmett1.2miligrams@gmail.com>
2018-08-28 23:33:14 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,7 +8,7 @@ INDEX_DIR=/var/lib/scratchpkg
 PORT_DIR=/usr/ports
 
 mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${HOOK_DIR},${PORT_DIR}}
-install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist scratch ${DESTDIR}${BINDIR}
+install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist pkglibdepends scratch ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
 mkdir -pv ${DESTDIR}${INDEX_DIR}/index
