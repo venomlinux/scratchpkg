b779c3a3a54faff3b28a31040001f69678c6d63e
emmett1 <emmett1.2miligrams@gmail.com>
2019-04-17 17:31:44 +0800

    add exclude libraries for revdep

--- INSTALL.sh
+++ INSTALL.sh
@@ -10,8 +10,8 @@ REVDEPD=/etc/revdep.d
 REVDEPCONF=/etc/revdep.conf
 
 mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${HOOK_DIR},${PORT_DIR},${REVDEPD}}
-touch ${DESTDIR}${REVDEPCONF}
 install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist pkglibdepends scratch pkgbase ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
 install -dm777 ${DESTDIR}${CACHE_DIR}/{packages,sources}
+install -m644 revdep.conf ${DESTDIR}${REVDEPCONF}
