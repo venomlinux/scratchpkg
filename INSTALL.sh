8a9c4eb185c4653e0b2bcfdf2934821711dde373
emmett1 <emmett1.2miligrams@gmail.com>
2019-05-01 16:42:01 +0800

    remove HOOK, add trigger builtin

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,14 +2,13 @@
 
 BINDIR=/usr/bin
 CONFDIR=/etc
-HOOK_DIR=/etc/hooks
 CACHE_DIR=/var/cache/scratchpkg
 INDEX_DIR=/var/lib/scratchpkg
 PORT_DIR=/usr/ports
 REVDEPD=/etc/revdep.d
 REVDEPCONF=/etc/revdep.conf
 
-mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${HOOK_DIR},${PORT_DIR},${REVDEPD}}
+mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${PORT_DIR},${REVDEPD}}
 install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist pkglibdepends scratch pkgbase ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
 install -m755 extra/* ${DESTDIR}${BINDIR}
