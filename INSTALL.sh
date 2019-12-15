4cebf3d7fa36d8706c29554f14015a35f8bcb193
emmett1 <emmett1.2miligrams@gmail.com>
2019-12-15 22:49:38 +0800

    cleanup repo, move extra scripts to scratchpkg-utils repo

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,8 +8,7 @@ REVDEPD=/etc/revdep.d
 REVDEPCONF=/etc/revdep.conf
 
 mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${PORT_DIR},${REVDEPD}}
-install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist pkglibdepends scratch pkgbase ${DESTDIR}${BINDIR}
+install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist scratch updateconf ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
-install -m755 extra/* ${DESTDIR}${BINDIR}
 install -dm777 ${DESTDIR}${CACHE_DIR}/{packages,sources,log,work}
 install -m644 revdep.conf ${DESTDIR}${REVDEPCONF}
