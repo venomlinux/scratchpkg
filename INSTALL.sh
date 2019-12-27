635c7371594ef09db1a426166d031802cd9a72ce
emmett1 <emmett1.2miligrams@gmail.com>
2019-12-27 15:07:54 +0800

    remove md5sum=() and use .checksums instead

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,7 +8,7 @@ REVDEPD=/etc/revdep.d
 REVDEPCONF=/etc/revdep.conf
 
 mkdir -pv ${DESTDIR}{${BINDIR},${CONFDIR},${PORT_DIR},${REVDEPD}}
-install -m755 revdep pkgadd pkgdel pkgbuild pkgdeplist scratch updateconf ${DESTDIR}${BINDIR}
+install -m755 revdep pkgadd pkgdel pkgbuild scratch updateconf ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo ${DESTDIR}${CONFDIR}
 install -dm777 ${DESTDIR}${CACHE_DIR}/{packages,sources,log,work}
 install -m644 revdep.conf ${DESTDIR}${REVDEPCONF}
