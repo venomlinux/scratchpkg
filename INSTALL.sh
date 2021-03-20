e2588ff9c8c6b372a7b1d8b836f4a64636f18dfb
emmett1 <emmett1.2miligrams@gmail.com>
2021-03-20 23:22:24 +0800

    updated

--- INSTALL.sh
+++ INSTALL.sh
@@ -16,7 +16,7 @@ install -dm777 ${DESTDIR}${CACHE_DIR}/packages
 install -dm777 ${DESTDIR}${CACHE_DIR}/sources
 install -dm777 ${DESTDIR}${CACHE_DIR}/work
 
-install -m755 xchroot revdep pkgadd pkgdel pkgbuild scratch updateconf reposync \
+install -m755 xchroot revdep pkgadd pkgdel pkgbuild scratch updateconf portsync \
               pkgbase pkgdepends pkgrebuild pkgfix portcreate ${DESTDIR}${BINDIR}
 install -m644 scratchpkg.conf scratchpkg.repo scratchpkg.alias scratchpkg.mask ${DESTDIR}${CONFDIR}
 
